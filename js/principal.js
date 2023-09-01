92e731f3a753ac74761cca490a5b882737f51a28
mih-18 <132575067+mih-18@users.noreply.github.com>
2023-09-01 09:34:45 -0300

    Add files via upload

---
+++ js/principal.js
@@ -0,0 +1,11 @@
+//atribui o valor da tag h1 para a variável título
+let titulo = document.querySelector("h1").textContent;
+//mostra no console do navegador a variável título
+console.log(titulo);
+//modifica o valor do título para a frase "Fica grandão birlllll"
+titulo.textContent = "Fica grandão birlllll";
+
+//------aqui vamos mexer na tabela de imc------
+
+let paciente = document.querySelector("#primeiro-paciente");
+console.log(paciente);
