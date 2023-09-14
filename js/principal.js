61610f3c5926ac489b24c47f6c8f22979cef2bdc
mih-18 <132575067+mih-18@users.noreply.github.com>
2023-09-14 12:13:36 -0300

    Add files via upload

--- js/principal.js
+++ js/principal.js
@@ -6,6 +6,41 @@ console.log(titulo);
 titulo.textContent = "Fica grandão birlllll";
 
 //------aqui vamos mexer na tabela de imc------
-
+//armazena na variável paciente as informações do primeiro paciente
 let paciente = document.querySelector("#primeiro-paciente");
 console.log(paciente);
+//armazena somente as colunas de peso e altura
+let tdPeso = paciente.querySelector (".info-peso");
+let tdAltura = paciente.querySelector (".info-altura");
+//armazena somente as informações de texto de peso e altura
+let peso = tdPeso.textContent;
+let altura = tdAltura.textContent;
+
+//acessa a coluna imc e altera o valor para o resultado do cálculo
+let tdImc = paciente.querySelector(".info-imc");
+
+//validação dos dados
+let pesoEhValido = true;
+let alturaEhValida = true;
+
+if(peso < 0 || peso > 1000){
+     console.log("Peso inválido!");   
+     tdImc.textContent = "Peso inválido!";
+     pesoEhValido = false;
+}
+
+if(altura < 0 || altura > 3){
+    console.log("Altura inválida!");   
+    tdImc.textContent = "Altura inválida!";
+    alturaEhValida = false;
+}
+
+if(pesoEhValido === true && alturaEhValida === true){
+    //realiza o cálculo do imc
+    let imc = peso / (altura * altura);
+    console.log(imc);
+    tdImc.textContent = imc;
+} else {
+    tdImc.textContent = "Peso e/ou altura inválidos!";
+}
+
